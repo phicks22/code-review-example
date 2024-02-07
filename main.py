45cd48dd64bae55521182d52a6cf225c0d39076b
phicks22 <65268541+phicks22@users.noreply.github.com>
2024-02-07 10:43:33 -0700

    added documentation and fixed variable names

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ import numpy as np
 from patblib import Path
 
 
-def z(x, y):
-    x = np.array(x)
-    return (y - np.mean(x)) / np.std(x)
+def z_score(arr: np.array, y: float) -> float:
+    """This function computes a z-score for a value against an array
+
+    :param arr: array to compute z-score against
+    :param y: the value to compute z-score for
+    :return z: the z-score for value y against the array
+    """
+
+    z = (y - np.mean(arr)) / np.std(arr)
+
+    return z
 
