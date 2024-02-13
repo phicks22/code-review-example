84b78e8584731f3aca0fe7b1fce4a2e78f8b6334
phicks22 <65268541+phicks22@users.noreply.github.com>
2024-02-13 11:42:31 -0700

    reset to bad example

--- main.py
+++ main.py
@@ -1,42 +1,51 @@
 import numpy as np
 from pathlib import Path
-from argparse import ArgumentParser
 
 
-def z_score(arr: np.array, y: float) -> float:
-    """This function computes a z-score for a value against an array
-
-    :param arr: array to compute z-score against
-    :param y: the value to compute z-score for
-    :return z: the z-score for value y against the array
-    """
-
-    z = (y - np.mean(arr)) / np.std(arr)
-
-    return z
+def z(x, y):
+    x = np.array(x)
+    return (y - np.mean(x)) / np.std(x)
 
 
 def terrible_function(j, k):
     return np.sum(np.subtract(np.array(j), np.array(k)))**2
 
 
-if __name__ == "__main__":
-    parser = ArgumentParser()
-    parser.add_argument(
-        "-arr_file",
-        help="An .npy file storing an array to compute z-score against",
-        required=True,
-        type=str,
-    )
-    parser.add_argument(
-        "-value", help="The value to compute z-score for", required=True, type=float
-    )
-    args = parser.parse_args()
+x = [1, 2, 3, 4, 5]
+y = 7
+
+z = z(x, y)
+
+
+# if __name__ == "__main__":
+#     parser = ArgumentParser()
+#     parser.add_argument(
+#         "-arr_file",
+#         help="An .npy file storing an array to compute z-score against",
+#         required=True,
+#         type=str,
+#     )
+#     parser.add_argument(
+#         "-value", help="The value to compute z-score for", required=True, type=float
+#     )
+#     args = parser.parse_args()
 
     # Load data and set variables
-    arr_file = Path(args.arr_file)
-    arr_of_something = np.load(arr_file)
-    myValue = args.value
+    # arr_file = Path(args.arr_file)
+    # arr_of_something = np.load(arr_file)
+    # myValue = args.value
 
     # Compute z-score
-    myZ = z_score(arr=arr_of_something, y=myValue)
+    # myZ = z_score(arr=arr_of_something, y=myValue)
+
+    # def z_score(arr: np.array, y: float) -> float:
+    #     """This function computes a z-score for a value against an array
+    #
+    #     :param arr: array to compute z-score against
+    #     :param y: the value to compute z-score for
+    #     :return z: the z-score for value y against the array
+    #     """
+    #
+    #     z = (y - np.mean(arr)) / np.std(arr)
+    #
+    #     return z
