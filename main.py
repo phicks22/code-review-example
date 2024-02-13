acf23361b101e51d32217b93b0032405af73c68d
phicks22 <65268541+phicks22@users.noreply.github.com>
2024-02-13 13:51:27 -0700

    added terrible function

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ def z(x, y):
     return (y - np.mean(x)) / np.std(x)
 
 
+def terrible_function(j, k):
+    return (np.sum(np.subtract(np.array(j), np.array(k)))**2) / len(j)
+
+
 x = [1, 2, 3, 4, 5]
 y = 7
 
